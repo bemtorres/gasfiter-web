ba7cd0131f9960abfc75796c7316377d05c2e1a2
Benjamin <benja.mora.torres@gmail.com>
2019-02-13 11:50:05 -0300

    update

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 
 
-const CACHE_STATIC_NAME  = 'static-v2';
-const CACHE_DYNAMIC_NAME = 'dynamic-v2';
-const CACHE_INMUTABLE_NAME = 'inmutable-v2';
+const CACHE_STATIC_NAME  = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 
 const CACHE_DYNAMIC_LIMIT = 50;
 
